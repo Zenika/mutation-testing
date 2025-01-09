7e8714dc7d4f5ace8be7e38b2d63ea0428f28321
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2025-01-09 17:14:24 +0100

    :tada: Premier méthode et test isAdulte

---
+++ src/main/java/com/zenika/AgeUtils.java
@@ -0,0 +1,9 @@
+package com.zenika;
+
+public class AgeUtils {
+    private AgeUtils() {
+    }
+    static boolean isAdulte(int age) {
+        return age >= 18;
+    }
+}
---
+++ src/test/java/com/zenika/AgeUtilsTest.java
@@ -0,0 +1,32 @@
+package com.zenika;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AgeUtilsTest {
+
+    @Test
+    void isAdulte_should_return_true_when_age_is_over_18() {
+        // Given
+        int age = 20;
+
+        // When
+        boolean result = AgeUtils.isAdulte(age);
+
+        // Then
+        assertTrue(result);
+    }
+
+    @Test
+    void isAdulte_should_return_false_when_age_is_under_18() {
+        // Given
+        int age = 17;
+
+        // When
+        boolean result = AgeUtils.isAdulte(age);
+
+        // Then
+        assertFalse(result);
+    }
+}
