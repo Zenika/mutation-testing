9c17232de62978ed6271b3b91950d8a994ea8632
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2025-01-20 17:40:22 +0100

    :white_check_mark: Fix les tests de PileTest

--- src/test/java/com/zenika/PileTest.java
+++ src/test/java/com/zenika/PileTest.java
@@ -9,19 +9,31 @@ public class PileTest {
     @Test
     public void testPushPop() {
         Pile pile = new Pile(3);
-        pile.push(0);
-        assertEquals(0, pile.pop());
+        assertThrows(IllegalStateException.class, pile::pop);
+        pile.push(1);
+        pile.push(2);
+        pile.push(3);
+        assertEquals(3, pile.pop());
+        assertEquals(2, pile.pop());
+        assertEquals(1, pile.pop());
     }
 
     @Test
     public void testIsEmpty() {
-        Pile pile = new Pile(0);
+        Pile pile = new Pile(3);
+        assertTrue(pile.isEmpty());
+        pile.push(1);
+        assertFalse(pile.isEmpty());
+        pile.pop();
         assertTrue(pile.isEmpty());
     }
 
     @Test
     public void testIsFull() {
-        Pile pile = new Pile(1);
-        assertFalse(pile.isFull());
+        Pile pile = new Pile(2);
+        pile.push(1);
+        pile.push(2);
+        assertTrue(pile.isFull());
+        assertThrows(IllegalStateException.class, () -> pile.push(3));
     }
 }
